81c7ae99f5830765b9dca0d3bc1036d1b2b3a93e
Nash43 <nyashjovo@gmail.com>
2023-11-02 12:07:35 -0700

    there is a part in the code causing an infinite loop, which one is it

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+* i = 0; is causing the infinite loop as there is no condition
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
