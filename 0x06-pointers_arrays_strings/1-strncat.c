f6626250f106997c60e739283f5390c0eb5dc1d9
Nash43 <nyashjovo@gmail.com>
2023-11-09 04:06:03 -0800

    a function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,34 +1,26 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
-  * _strncat  - the main function
-  *
-  * @n: Function parameter
-  *
-  * @src: Function parameter
-  *
-  * @dest: Function parammeter
-  *
-  * Return: dest
-  */
-char *_strncat(char *dest, char *src, int n);
+ * *_strncat - concatenates n bytes of two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of bytes
+ * Return: void
+ */
+
+char *_strncat(char *dest, char *src, int n)
 {
-	int k, r;
 
-	k = 0;
-	r = 0;
+int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+;
 
-	while (dest[k] != '\0')
-	{
-	k++;
-	}
-	while (r < n && src[r] != '\0')
-	{
-	dest[k] = src[r];
-	k++;
-	r++;
-	}
-	dest[k] = '\0';
-	return (dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
+
+/*should end with a end of string char*/
+dest[dest_len + i] = '\0';
+
+return (dest);
 }
 
