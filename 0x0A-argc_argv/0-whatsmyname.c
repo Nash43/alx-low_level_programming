f94321af6e0062e3ff848ff2468d6150c03fedb7
Nash43 <nyashjovo@gmail.com>
2023-11-21 04:20:33 -0800

    c command line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main: prints out the name of the file
+ * return 0: success
+ */
+
+int main (int argc, char* argv[])
+{
+	printf(" argv[] = %s\n", argv[0]);
+	return 0;
+}
