91470564288edf6549d82d47109c384338d8f974
Nash43 <nyashjovo@gmail.com>
2023-11-22 03:09:32 -0800

    a programme that prints out the name of the file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int a, b, c; // declare three integer variables
+    printf("Enter two numbers: "); // prompt the user to enter two number
+    c = a * b;
+	printf("The product of %d and %d is %d\n", a, b, c);
+	return 0;
+}
